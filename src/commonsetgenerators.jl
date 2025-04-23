17579997317c00961d980a9f36d257181021fce2
Ian George <qit.george@gmail.com>
2025-04-23 21:21:18 +0800

    added documentation; updated docstrings

--- src/commonsetgenerators.jl
+++ src/commonsetgenerators.jl
@@ -1,37 +1,4 @@
 """
-    genGellMan(d)
-
-This function constructs and returns the generalized Gell Mann matrices.
-"""
-function genGellMann(d)
-    genGMmats = Matrix{Complex}[]
-    for n = 1:d
-        for m = 1:d
-            if n==d && m == d
-                nothing
-            elseif n == m && n != d
-                Gnn = zeros(d,d)
-                for i = 1:n
-                    Gnn[i,i] = 1
-                end
-                Gnn[n+1,n+1] = -n
-                Gnn = 1/sqrt(n*(n+1))*Gnn
-                push!(genGMmats,Gnn)
-            elseif n < m
-                Enm = zeros(d,d)
-                Enm[n,m] = 1
-                push!(genGMmats,1/sqrt(2)*(Enm + Enm'))
-            else
-                Enm = zeros(d,d)
-                Enm[n,m] = 1
-                push!(genGMmats,1im/sqrt(2)*(Enm - Enm'))
-            end
-        end
-    end
-    return genGMmats
-end
-
-"""
     gencompbasis(d)
 
 This function returns the computational basis of dimension d.
@@ -73,4 +40,37 @@ function genNormDiscWeyl(d)
         end
     end
     return Wab
+end
+
+"""
+    genGellMan(d)
+
+This function constructs and returns the generalized Gell Mann matrices for dimension d.
+"""
+function genGellMann(d)
+    genGMmats = Matrix{Complex}[]
+    for n = 1:d
+        for m = 1:d
+            if n==d && m == d
+                nothing
+            elseif n == m && n != d
+                Gnn = zeros(d,d)
+                for i = 1:n
+                    Gnn[i,i] = 1
+                end
+                Gnn[n+1,n+1] = -n
+                Gnn = 1/sqrt(n*(n+1))*Gnn
+                push!(genGMmats,Gnn)
+            elseif n < m
+                Enm = zeros(d,d)
+                Enm[n,m] = 1
+                push!(genGMmats,1/sqrt(2)*(Enm + Enm'))
+            else
+                Enm = zeros(d,d)
+                Enm[n,m] = 1
+                push!(genGMmats,1im/sqrt(2)*(Enm - Enm'))
+            end
+        end
+    end
+    return genGMmats
 end
